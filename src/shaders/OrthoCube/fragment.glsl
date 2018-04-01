3d272e12ca6f0f11de8698c3d391acd6ee3d0bcd
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2018-04-01 13:00:06 +0200

    Raymarch reveal orthocube

--- src/shaders/OrthoCube/fragment.glsl
+++ src/shaders/OrthoCube/fragment.glsl
@@ -32,6 +32,12 @@ float sphere(vec3 p, float s) {
     return length(p)-s;
 }
 
+float dissphere( vec3 p, float s)
+{
+    float d1 = sphere(p, s);
+    float d2 = sin(1.2 * p.x) * sin(1.2 * p.y) * sin(2.2 * p.z);
+    return d1+d2;
+}
 
 vec2 sphereCube(vec3 p, float sphereSize, float gap){
     float d = END + EPSILON;
@@ -42,7 +48,7 @@ vec2 sphereCube(vec3 p, float sphereSize, float gap){
     for(float x = -1.0; x <= 1.0; x++){
         for(float y = -1.0; y <= 1.0; y++){
             for(float z = -1.0; z <= 1.0; z++){
-                cD = sphere((p-vec3(x*gap, y*gap, z*gap)), sphereSize);
+                cD = dissphere((p-vec3(x*gap, y*gap, z*gap)), sphereSize);
                 if(cD < d){
                     d = cD;
                     currentID = oID;
@@ -135,6 +141,59 @@ vec3 estimateNormal(vec3 p) {
     ));
 }
 
+vec3 rayDir(float fov, vec2 uv) {
+    vec2 xy = uv * 2.0 - 1.0;
+    xy.y = xy.y / (16.0 / 9.0);
+    float z = 2.0 / tan(radians(fov / 2.0));
+    return normalize(vec3(xy, -z));
+}
+
+
+vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
+        vec3 lightPos, vec3 lightIntensity) {
+    vec3 N = estimateNormal(p);
+    vec3 L = normalize(lightPos - p);
+    vec3 V = normalize(eye - p);
+    vec3 R = normalize(reflect(-L, N));
+
+    float dotLN = dot(L, N);
+    float dotRV = dot(R, V);
+
+    if (dotLN < 0.0) {
+        return vec3(0.0, 0.0, 0.0);
+    } 
+
+    if (dotRV < 0.0) {
+        return lightIntensity * (k_d * dotLN);
+    }
+    return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+}
+
+vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
+
+    vec3 pos = p;
+    vec3 nor = estimateNormal(p);
+    vec3 rd = eye;
+    vec3 ref = reflect( rd, nor );
+    vec3  lig = normalize( vec3(-0.4, 0.7, -0.6) );
+    float amb = clamp( 0.5+0.5*nor.y, 0.0, 1.0 );
+    float dif = clamp( dot( nor, lig ), 0.0, 1.0 );
+    float bac = clamp( dot( nor, normalize(vec3(-lig.x,0.0,-lig.z))), 0.0, 1.0 )*clamp( 1.0-pos.y,0.0,1.0);
+    float fre = pow( clamp(1.0+dot(nor,rd),0.0,1.0), 2.0 );
+    float spe = pow(clamp( dot( ref, lig ), 0.0, 1.0 ),16.0);
+
+    vec3 lin = vec3(0.0);
+    lin += 1.30*dif*vec3(1.00,0.80,0.55);
+    lin += 2.00*spe*vec3(1.00,0.90,0.70)*dif;
+    lin += 0.40*amb*vec3(0.40,0.60,1.00);
+    lin += 0.50*bac*vec3(0.25,0.25,0.25);
+    lin += 0.25*fre*vec3(1.00,1.00,1.00);
+    vec3 color = k_a * lin;
+
+    return color;
+}
+
+
 void main() {
     vec3 color1 = vec3(255.0/255.0, 73.0/255.0, 130.0/255.0);
     vec3 color2 = vec3(119.0/255.0, 225.0/255.0, 93.0/255.0);
@@ -142,15 +201,15 @@ void main() {
     vec3 dark   = vec3(55.0/255.0, 60.0/255.0, 63.0/255.0);
 
     //vec3 pos = vec3(cos(frame/20.0)*10.0, 5.0, sin(frame/20.0)*10.0);
-    vec3 pos = vec3(0.0, 0.0, -10.0);
-    vec3 dir = normalize(-pos);
+    vec3 pos = vec3(0.0, 0.0, 10.0);
+    vec3 dir = rayDir(120.0, vUv);
 
     vec2 propUV = vUv - vec2(0.5, 0.5);
     propUV = propUV * vec2(16.0, 9.0) * 1.4;
 
     //NOTE: Math assumes camera is always pointing in Z+
     //do not rotate the camera without fixing math
-    vec3 eye = pos + vec3(propUV, 0.0);
+    vec3 eye = pos ;
 
     vec3  distOid = march(eye, dir, START, END);
     float dist = distOid.x;
@@ -166,12 +225,14 @@ void main() {
         vec3 normal = estimateNormal(p);
         float fresnell = abs(dot(dir,normal));
 
+        fresnell = 1.0;
         if(fresnell < 0.5){ //border!
             color = dark; 
         }else{
             float beat = (BEAN - 240.0)/12.0;
             color = mod(oID, 2.0) == 0.0 ? white : color2;
 
+            color = phongIllumination(color, color, normalize(vec3(1.0, 1.0, 1.0)), 10.0, p, eye);
             //Morph to white at the end
             color += vec3(1.0, 1.0, 1.0) * pow(max(0.0 , (beat-15.0) * 2.0), 2.0);
         }
